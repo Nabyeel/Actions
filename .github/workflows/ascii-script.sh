05d87c18689f49d66ae9fd046c07c38fde2862d3
Nabyeel <33094138+Nabyeel@users.noreply.github.com>
2026-01-15 16:29:22 +0500

    Update ascii-script.sh

--- .github/workflows/ascii-script.sh
+++ .github/workflows/ascii-script.sh
@@ -1,1 +1,14 @@
+ #Install cowsay program
+    sudo apt-get install cowsay -y
+        
+   # Execute Cowsay cmd
+   cowsay -f dragon "Run for cover, I am a dragon...RAWR" >> dragon.txt
 
+ #Text file exist
+  grep -i "dragon" dragon.txt
+
+    # Read File
+cat dragon.txt
+
+  # checkout Repo Files
+     ls -ltra
